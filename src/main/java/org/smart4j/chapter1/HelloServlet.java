d3f361cd72d6a3f4993ddd3394b094e27259a622
yany0630 <yanyidl@cn.ibm.com>
2016-01-06 13:42:33 +0100

    my first git app

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,27 @@
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by root on 1/6/16.
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        request.setAttribute("currentTime", currentTime);
+        System.out.print("test");
+        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
+
+    }
+}
